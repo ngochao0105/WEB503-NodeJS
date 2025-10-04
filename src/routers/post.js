e59e9091b2cd6fe3d79b617d29940545c586d6a8
ngochao0105 <ngochao21012005@gmail.com>
2025-10-04 21:50:17 +0700

    ASM1

--- src/routers/post.js
+++ src/routers/post.js
@@ -5,5 +5,8 @@ const router = Router();
 
 router.get("/", getPosts);
 router.post("/", addPost);
+router.get("/:id", getPostById);
+router.put("/:id", updatePost);
+router.delete("/:id", deletePost);
 
 export default router;
